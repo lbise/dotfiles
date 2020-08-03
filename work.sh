7fba2ccf41adc570f41bb4ae7ce255c6efdca68a
Léonard Bise <leonard.bise@gmail.com>
2020-08-03 11:35:11 +0200

    work: Add evolution package

--- work.sh
+++ work.sh
@@ -2,12 +2,17 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
+PKGS="evolution evolution-ews"
 AUR_PKGS="netextender"
 
 # Commands
 ENABLE_SERVICE="systemctl enable "
 INSTALL_CMD="pacman -S --needed "
 AUR_INSTALL_CMD="yay -S --needed "
+
+echo "Installing packages: $PKGS"
+sudo $INSTALL_CMD $PKGS
+echo "-------------------------------------------------------------------------"
 
 echo "Installing AUR packages: $AUR_PKGS"
 $AUR_INSTALL_CMD $AUR_PKGS
