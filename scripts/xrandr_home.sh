16163700326b8a2725adbaa4d5c3709fcb6393b1
Léonard Bise <leonard.bise@gmail.com>
2020-08-10 10:40:48 +0200

    display: Fix display use

--- scripts/xrandr_home.sh
+++ scripts/xrandr_home.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-xrandr --output DVI-I-1 --primary --mode 1920x1080 --pos 2560x0 --rotate normal --output DVI-D-1 --off --output HDMI-1 --off --output DP-1 --mode 2560x1440 --pos 0x0 --rotate normal
+xrandr --output DVI-I-0 --off --output DVI-I-1 --mode 1920x1080 --pos 2560x0 --rotate normal --output HDMI-0 --off --output DP-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DVI-D-0 --off
