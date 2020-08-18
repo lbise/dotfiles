ce1c297ce331dfff2c3ef30078b8c8a184f4850f
Léonard Bise <leonard.bise@gmail.com>
2020-08-18 10:46:32 +0200

    lightdm: Update slick-greeter config

--- symlinks.sh
+++ symlinks.sh
@@ -41,12 +41,14 @@ sudo $DIR/tools/new_symlink.sh $DIR/00-keyboard.conf  /etc/X11/xorg.conf.d/00-ke
 $DIR/tools/new_symlink.sh $DIR/config/kitty/kitty.conf ~/.config/kitty/kitty.conf
 $DIR/tools/new_symlink.sh $DIR/nord-kitty/nord.conf ~/.config/kitty/nord.conf
 
-# lightdm
-sudo $DIR/tools/new_symlink.sh $DIR/lightdm.conf  /etc/lightdm/lightdm.conf
-sudo $DIR/tools/new_symlink.sh $DIR/scripts/display_setup.sh  /usr/share/display_setup.sh
-sudo $DIR/tools/new_symlink.sh $DIR/scripts/xrandr_home.sh  /usr/share/xrandr_home.sh
-sudo $DIR/tools/new_symlink.sh $DIR/scripts/xrandr_docked.sh  /usr/share/xrandr_docked.sh
-sudo $DIR/tools/new_symlink.sh $DIR/scripts/xrandr_laptop.sh  /usr/share/xrandr_laptop.sh
+# lightdm - files need to be copied
+sudo cp $DIR/lightdm.conf /etc/lightdm/lightdm.conf
+sudo cp $DIR/slick-greeter.conf /etc/lightdm/slick-greeter.conf
+sudo cp $DIR/greeter_background.jpg /usr/share/pixmaps/greeter_background.jpg
+sudo cp $DIR/scripts/display_setup.sh /usr/share/display_setup.sh
+sudo cp $DIR/scripts/xrandr_home.sh /usr/share/xrandr_home.sh
+sudo cp $DIR/scripts/xrandr_docked.sh /usr/share/xrandr_docked.sh
+sudo cp $DIR/scripts/xrandr_laptop.sh /usr/share/xrandr_laptop.sh
 
 # scripts
 sudo $DIR/tools/new_symlink.sh $DIR/scripts  ~/.scripts
