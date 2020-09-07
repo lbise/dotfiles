e75c8b792b769124606d1bba82bd9606de2a4968
Léonard Bise <leonard.bise@gmail.com>
2020-09-07 12:07:58 +0200

    scripts: Improve xrandr_home

--- scripts/xrandr_home.sh
+++ scripts/xrandr_home.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-xrandr --output DVI-I-0 --off --output DVI-I-1 --mode 1920x1080 --pos 2560x0 --rotate normal --output HDMI-0 --off --output DP-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DVI-D-0 --off
+PRIM_DISPLAY="DP-1"
+PRIM_RES="2560x1440"
+SEC_DISPLAY="DVI-I-1"
+SEC_RES="1920x1080"
+
+xrandr --output $PRIM_DISPLAY --primary --mode $PRIM_RES --rotate normal --output $SEC_DISPLAY --mode $SEC_RES --rotate normal --right-of $PRIM_DISPLAY
+#xrandr --output DVI-I-1 --mode 1920x1080 --pos 2560x0 --rotate normal --output HDMI-0 --off --output DP-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DVI-D-0 --off
