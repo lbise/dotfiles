280ae3e6adad88cd01e47046e9b16e22ef4e2687
Léonard Bise <leonard.bise@gmail.com>
2020-09-10 09:03:37 +0200

    scripts: Query xrandr for connected displays

--- scripts/xrandr_home.sh
+++ scripts/xrandr_home.sh
@@ -1,8 +1,23 @@
 #!/usr/bin/env bash
-PRIM_DISPLAY="DP-1"
 PRIM_RES="2560x1440"
-SEC_DISPLAY="DVI-I-1"
 SEC_RES="1920x1080"
 
-xrandr --output $PRIM_DISPLAY --primary --mode $PRIM_RES --rotate normal --output $SEC_DISPLAY --mode $SEC_RES --rotate normal --right-of $PRIM_DISPLAY
-#xrandr --output DVI-I-1 --mode 1920x1080 --pos 2560x0 --rotate normal --output HDMI-0 --off --output DP-0 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1 --off --output DVI-D-0 --off
+DO_AUTO=false
+# Primary display is connected on display port and secondary on DVI
+PRIM_DISPLAY=`xrandr | grep " connected " | grep "DP" | awk '{ print $1 }'`
+if [ !"$PRIM_DISPLAY" == "DP*" ]; then
+	echo "Cannot determine primary display ($PRIM_DISPLAY)"
+	DO_AUTO=true
+fi
+
+SEC_DISPLAY=`xrandr | grep " connected " | grep "DVI" | awk '{ print $1 }'`
+if [ !"$SEC_DISPLAY" == "DVI*" ]; then
+	echo "Cannot determine secondary display ($SEC_DISPLAY)"
+	DO_AUTO=true
+fi
+
+if [ $DO_AUTO = true  ]; then
+	xrandr --auto
+else
+	xrandr --output $PRIM_DISPLAY --primary --mode $PRIM_RES --rotate normal --output $SEC_DISPLAY --mode $SEC_RES --rotate normal --right-of $PRIM_DISPLAY
+fi
