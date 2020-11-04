ff27ab0c47de56e90be92261b4b29e89fe413dca
Léonard Bise <leonard.bise@gmail.com>
2020-11-04 16:12:59 +0100

    scripts: Improve xrandr home display script

--- scripts/xrandr_home.sh
+++ scripts/xrandr_home.sh
@@ -5,23 +5,37 @@ SEC_RES="1920x1080"
 SEC_PORT="HDMI"
 
 DO_AUTO=false
-# Primary display is connected on display port and secondary on DVI
-PRIM_DISPLAY=`xrandr | grep " connected " | grep "$PRIM_PORT" | awk '{ print $1 }'`
-if [ "$PRIM_DISPLAY" = "" ] || [ !"$PRIM_DISPLAY" == "$PRIM_PORT*" ]; then
-	echo "Cannot determine primary display ($PRIM_DISPLAY)"
-	DO_AUTO=true
-fi
 
-SEC_DISPLAY=`xrandr | grep " connected " | grep "$SEC_PORT" | awk '{ print $1 }'`
-if [ "$SEC_DISPLAY" = "" ] || [ !"$SEC_DISPLAY" == "$SEC_PORT*" ]; then
-	echo "Cannot determine secondary display ($SEC_DISPLAY)"
-	DO_AUTO=true
-fi
+while read -r line ; do
+	if [[ $line == *"$PRIM_RES"* ]]; then
+		PRIM_DISPLAY=`echo $line | head -n1 | awk '{ print $1 }'`
+	elif [[ $line == *"$SEC_RES"* ]]; then
+		SEC_DISPLAY=`echo $line | head -n1 | awk '{ print $1 }'`
+	fi
+done < <(xrandr | grep " connected ")
 
-if [ $DO_AUTO = true  ]; then
-	xrandr --auto
+if [ "$PRIM_DISPLAY" != "" ]; then
+	echo "Primary display: \"$PRIM_DISPLAY\""
 else
-	echo "Primary display: $PRIM_DISPLAY"
-	echo "Secondary display: $SEC_DISPLAY"
+	echo "Cannot determine primary display"
+fi
+
+if [ "$SEC_DISPLAY" != "" ]; then
+	echo "Secondary display: \"$SEC_DISPLAY\""
+else
+	echo "Cannot determine secondary display"
+fi
+
+if [ "$PRIM_DISPLAY" != "" ] && [ "$SEC_DISPLAY" != "" ]; then
+	echo "Configuring primary and secondary display"
 	xrandr --output $PRIM_DISPLAY --primary --mode $PRIM_RES --rotate normal --output $SEC_DISPLAY --mode $SEC_RES --rotate normal --right-of $PRIM_DISPLAY
+elif [ "$PRIM_DISPLAY" != "" ] && [ "$SEC_DISPLAY" == "" ]; then
+	echo "Configuring only primary"
+	xrandr --output $PRIM_DISPLAY --primary --mode $PRIM_RES --rotate normal
+elif [ "$PRIM_DISPLAY" == "" ] && [ "$SEC_DISPLAY" != "" ]; then
+	echo "Configuring only secondary"
+	xrandr --output $SEC_DISPLAY --mode $SEC_RES --rotate normal
+else
+	echo "Configuring auto"
+	xrandr --auto
 fi
