837de3016ae1f8a2790d3fcb713316101c955f81
Léonard Bise <leonard.bise@gmail.com>
2021-02-17 14:05:30 +0100

    Split common symlinks from others

--- symlinks.sh
+++ symlinks.sh
@@ -2,37 +2,15 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
+$DIR/symlinks_common.sh
+
 # rxvt
 $DIR/tools/new_symlink.sh $DIR/.Xresources ~/.Xresources
-
-# vim
-$DIR/tools/new_symlink.sh $DIR/.vimrc ~/.vimrc
-$DIR/tools/new_symlink.sh $DIR/vim/ftplugin ~/.vim/ftplugin
-$DIR/tools/new_symlink.sh $DIR/vim/pack ~/.vim/pack
-$DIR/tools/new_symlink.sh $DIR/vim/colorschemes/nord-vim/colors/nord.vim ~/.vim/colors/nord.vim
-$DIR/tools/new_symlink.sh $DIR/vim/colorschemes/nord-vim/autoload/airline/themes/nord.vim ~/.vim/autoload/airline/themes/nord.vim
-
-# git
-$DIR/tools/new_symlink.sh $DIR/.gitconfig ~/.gitconfig
-$DIR/tools/new_symlink.sh $DIR/githooks ~/.githooks
-
-# Exuberant ctags
-$DIR/tools/new_symlink.sh $DIR/.ctags ~/.ctags
-# Universal ctags
-$DIR/tools/new_symlink.sh $DIR/.ctags ~/.ctags.d/default.ctags
-
-# gdb
-$DIR/tools/new_symlink.sh $DIR/gdb/gdb-dashboard/.gdbinit ~/.gdbinit
-$DIR/tools/new_symlink.sh $DIR/.gdbinit ~/.gdbinit.d/.gdbinit
 
 # i3
 $DIR/tools/new_symlink.sh $DIR/config/i3/config ~/.config/i3/config
 $DIR/tools/new_symlink.sh $DIR/config/rofi/config ~/.config/rofi/config
 sudo $DIR/tools/new_symlink.sh $DIR/rofi/nord-rofi/nord.rasi /usr/share/rofi/themes/nord.rasi
-
-# zsh
-$DIR/tools/new_symlink.sh $DIR/.zshrc  ~/.zshrc
-$DIR/tools/new_symlink.sh $DIR/powerlevel10k  ~/.oh-my-zsh/custom/themes/powerlevel10k
 
 # X11
 sudo $DIR/tools/new_symlink.sh $DIR/00-keyboard.conf  /etc/X11/xorg.conf.d/00-keyboard.conf
@@ -49,9 +27,6 @@ sudo cp $DIR/scripts/display_setup.sh /usr/share/display_setup.sh
 sudo cp $DIR/scripts/xrandr_home.sh /usr/share/xrandr_home.sh
 sudo cp $DIR/scripts/xrandr_docked.sh /usr/share/xrandr_docked.sh
 sudo cp $DIR/scripts/xrandr_laptop.sh /usr/share/xrandr_laptop.sh
-
-# scripts
-sudo $DIR/tools/new_symlink.sh $DIR/scripts  ~/.scripts
 
 # polybar
 $DIR/tools/new_symlink.sh $DIR/config/polybar/config ~/.config/polybar/config
