dd7d768e2232dd100ab436f0a001fb013210182f
Leonard Bise <leonard.bise@phonak.com>
2021-03-25 08:26:15 +0100

    scripts: Add scripts to copy dotfiles

---
+++ scripts/df_to_repo.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+/c/Users/13lbise/gitrepo/dotfiles/tools/cp_files.sh /c/Users/13lbise/gitrepo/dotfiles/symlinks_common.cfg dst
+/c/Users/13lbise/gitrepo/dotfiles/tools/cp_files.sh /c/Users/13lbise/gitrepo/dotfiles/symlinks_win.cfg dst
