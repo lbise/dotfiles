96d478aefe63f10a49e7dca12c9a30d296ef5070
Leonard Bise <leonard.bise@sonova.com>
2022-09-01 10:52:11 +0200

    WindowsTerminal: Add script to retrieve config + update config

---
+++ win/get_config.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+DOTFILES="$DIR/.."
+
+WINTERMCFG="`wslpath "$(wslvar USERPROFILE)"`/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json"
+
+echo "-------------------------------------------------------------------------"
+echo "Update dotfiles Windows Terminal configuration file"
+cp $WINTERMCFG $DOTFILES/win/winterm.settings.json
+
+echo "#########################################################################"
+echo "Get config end"
+echo "#########################################################################"
