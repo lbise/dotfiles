9ffb6b8962b6a1284d9cebbeff11a244cabf1014
Leonard Bise <leonard.bise@sonova.com>
2023-10-23 14:50:55 +0200

    archives: Add nvim plugins

--- scripts/mount_phc.sh
+++ scripts/mount_phc.sh
@@ -1,8 +1,7 @@
 #!/bin/env bash
 
-sudo mkdir -p /mnt/p /mnt/t /mnt/u /mnt/ch03pool/murten_mirror
+sudo mkdir -p /mnt/p /mnt/t /mnt/u /mnt/z
 sudo mount -t drvfs P: /mnt/p
 sudo mount -t drvfs T: /mnt/t
 sudo mount -t drvfs u: /mnt/u
 sudo mount -t drvfs z: /mnt/z
-sudo mount -t drvfs z: /mnt/ch03pool/murten_mirror
