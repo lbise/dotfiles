1535d4d16e5e595c3d813a910e4734196ee5bc62
Leonard Bise <leonard.bise@sonova.com>
2023-10-30 14:44:34 +0100

    install: update neovim plugins

--- install.sh
+++ install.sh
@@ -24,8 +24,8 @@ ONEDRIVE_PATH="/mnt/c/Users/13lbise/OneDrive - Sonova"
 KEYS_SSH_DIR="$ONEDRIVE_PATH/.ssh"
 KEYS_GPG_DIR="$ONEDRIVE_PATH/.gnupg"
 COMMON_PACKAGES="zsh fzf ripgrep gzip tmux fd-find pinentry-tty"
-NVIM_PLUGINS_MD5="3a3c452a28463a2552169577cd83de03"
-
+NVIM_PLUGINS_MD5="e0959a24634ec6d7bceb340c75e7d78b"
+.
 function print_usage() {
     USAGE="$(basename "$0") [-h|--help] [-l|--linkonly] [-t|--test] -- Install dotfiles
 
