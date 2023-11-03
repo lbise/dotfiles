79a8d3b1f15b9b21f8ef2bbc331cdbeb57d82f44
Leonard Bise <leonard.bise@sonova.com>
2023-11-03 11:52:20 +0100

    neovim: Add ruff-lsp

--- nvim/lua/core/settings.lua
+++ nvim/lua/core/settings.lua
@@ -2,20 +2,17 @@ local settings = {
     colorscheme = 'tokyonight',
     lsp = {
         ensure_installed = {
-            clangd = {},
-            pyright = {},
-            bashls = {},
-            lua_ls = {
-                Lua = {
-                    workspace = { checkThirdParty = false },
-                    telemetry = { enable = false },
-                },
-            },
+            'clangd',
+            'pyright',
+            'ruff_lsp',
+            'bashls',
+            'lua_ls',
         },
         -- Servers enable status, can also be a list of options passed to  require('lspconfig')[server].setup()
         servers = {
             clangd = false,
             pyright = true,
+            ruff_lsp = true,
             bashls = true,
             lua_ls = {
                 workspace = {
