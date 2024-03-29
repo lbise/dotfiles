2f33762d5db1d1063096cefdaf4928d4a60ce4ae
Léonard Bise <leonard.bise@gmail.com>
2024-03-29 10:17:49 +0100

    scripts: add wow_backup.sh

---
+++ scripts/wow_backup.sh
@@ -0,0 +1,27 @@
+#!/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+WOW_WIN_PATH="/mnt/c/Program Files (x86)/World of Warcraft"
+WOW_SUBDIR_BACKUP=("_classic_era_/Interface/AddOns" "_classic_era_/WTF")
+OUTPUT_PATH="$HOME/gitrepo/wowaddons"
+
+if [ ! -d "$WOW_WIN_PATH" ]; then
+    echo "ERROR: $WOW_WIN_PATH does not exist"
+    exit 1
+fi
+
+ARCHIVE_NAME="wow_backup_$(date +%d%m%Y)_$(date +%s)"
+TMP_DST_DIR="$HOME/$ARCHIVE_NAME"
+mkdir $TMP_DST_DIR
+echo "Backing up to $TMP_DST_DIR"
+
+for subdir in "${WOW_SUBDIR_BACKUP[@]}"
+do
+    echo "Copying $WOW_WIN_PATH/$subdir"
+    cp -r "$WOW_WIN_PATH/$subdir" $TMP_DST_DIR
+done
+
+cd $HOME
+tar czf $OUTPUT_PATH/$ARCHIVE_NAME.tar.gz $ARCHIVE_NAME
+echo "Created archive $OUTPUT_PATH/$ARCHIVE_NAME.tar.gz"
+
+rm -rf $TMP_DST_DIR
