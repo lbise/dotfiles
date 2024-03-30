b39c68ebc8b84f3182474e76bd80dc643e7e8430
Léonard Bise <leonard.bise@gmail.com>
2024-03-30 13:14:08 +0100

    scripts: Do not backup all addons, only config

--- scripts/wow_backup.sh
+++ scripts/wow_backup.sh
@@ -1,7 +1,10 @@
 #!/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 WOW_WIN_PATH="/mnt/c/Program Files (x86)/World of Warcraft"
-WOW_SUBDIR_BACKUP=("_classic_era_/Interface/AddOns" "_classic_era_/WTF")
+ADDON_WIN_PATH="$WOW_WIN_PATH/_classic_era_/Interface/AddOns"
+# Do not backup Addons themselves, only config for now
+#WOW_SUBDIR_BACKUP=("_classic_era_/Interface/AddOns" "_classic_era_/WTF")
+WOW_SUBDIR_BACKUP=("_classic_era_/WTF")
 OUTPUT_PATH="$HOME/gitrepo/wowaddons"
 
 if [ ! -d "$WOW_WIN_PATH" ]; then
@@ -19,6 +22,9 @@ do
     echo "Copying $WOW_WIN_PATH/$subdir"
     cp -r "$WOW_WIN_PATH/$subdir" $TMP_DST_DIR
 done
+
+# Create list of addons
+ls "$ADDON_WIN_PATH" >> $TMP_DST_DIR/addons.txt
 
 cd $HOME
 tar czf $OUTPUT_PATH/$ARCHIVE_NAME.tar.gz $ARCHIVE_NAME
