cc5d80a689b2861c04301354baba423739621183
Leonard Bise <leonard.bise@sonova.com>
2024-05-24 12:29:15 +0200

    install: Update nvim plugins

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ KEYS_GPG_DIR="$ONEDRIVE_PATH/.gnupg"
 COMMON_PACKAGES="zsh fzf ripgrep gzip tmux curl wget unzip tar npm python3 pass"
 UBUNTU_COMMON_PACKAGES="fd-find pinentry-tty build-essential gdb"
 MAC_PACKAGES="fd gpg universal-ctags nvim"
-NVIM_PLUGINS_MD5="1d3f451d4ff831fb3a5941977e6d5b97"
+NVIM_PLUGINS_MD5="c08d3099466867572c0708fa030e2f16"
 GPG_KEYID="ED0DFB79FF83B277"
 
 function print_usage() {
