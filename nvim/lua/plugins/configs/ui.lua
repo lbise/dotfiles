40e40d99d1d8f2b5ce640185b64d2188440eb71b
Leonard Bise <leonard.bise@sonova.com>
2024-12-19 08:16:51 +0100

    nvim: Add snacks, replace noice and indent

--- nvim/lua/plugins/configs/ui.lua
+++ nvim/lua/plugins/configs/ui.lua
@@ -174,51 +174,25 @@ local config = {
         }
     },
     {
-        -- Add indentation guides even on blank lines
-        'lukas-reineke/indent-blankline.nvim',
-        event = { 'BufReadPost', 'BufNewFile' },
-        main = 'ibl',
+        "folke/snacks.nvim",
+        priority = 1000,
+        lazy = false,
+        ---@type snacks.Config
         opts = {
-            scope = { enabled = false },
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+            bigfile = { enabled = true },
+            dashboard = { enabled = true },
+            indent = { enabled = true },
+            input = { enabled = true },
+            notifier = { enabled = true },
+            quickfile = { enabled = true },
+            scroll = { enabled = true },
+            statuscolumn = { enabled = true },
+            words = { enabled = true },
         },
     },
-    {
-        'folke/noice.nvim',
-        event = 'VeryLazy',
-        opts = {
-          -- add any options here
-        },
-        dependencies = {
-          -- if you lazy-load any plugin below, make sure to add proper `module='...'` entries
-          'MunifTanjim/nui.nvim',
-          -- OPTIONAL:
-          --   `nvim-notify` is only needed, if you want to use the notification view.
-          --   If not available, we use `mini` as the fallback
-          'rcarriga/nvim-notify',
-        },
-        config = function()
-            require('noice').setup({
-                lsp = {
-                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                    override = {
-                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                        ["vim.lsp.util.stylize_markdown"] = true,
-                        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-                    },
-                },
-                presets = {
-                    bottom_search = true, -- use a classic bottom cmdline for search
-                    command_palette = false, -- position the cmdline and popupmenu together
-                    long_message_to_split = true, -- long messages will be sent to a split
-                    inc_rename = false, -- enables an input dialog for inc-rename.nvim
-                    lsp_doc_border = false, -- add a border to hover docs and signature help
-                },
-                cmdline = {
-                    view = "cmdline", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmd
-                }
-            })
-        end,
-    }
 }
 
 return config
