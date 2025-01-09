026431c0a4c915ec36b493af5fcaa1bcdd6cf303
Léonard Bise <leonard.bise@gmail.com>
2025-01-09 19:56:00 +0100

    blink.cmp

--- nvim/lua/plugins/configs/completion.lua
+++ nvim/lua/plugins/configs/completion.lua
@@ -5,74 +5,74 @@ local has_words_before = function()
 end
 
 local config = {
-    {
-        'hrsh7th/nvim-cmp',
-        event = { "InsertEnter", "CmdlineEnter" },
-        config = function()
-            require('cmp').setup({
-                snippet = {
-                    expand = function(args)
-                        require('luasnip').lsp_expand(args.body)
-                    end,
-                },
-                mapping = require('cmp').mapping.preset.insert {
-                    ['<C-n>'] = require('cmp').mapping.select_next_item(),
-                    ['<C-p>'] = require('cmp').mapping.select_prev_item(),
-                    ['<C-d>'] = require('cmp').mapping.scroll_docs(-4),
-                    ['<C-f>'] = require('cmp').mapping.scroll_docs(4),
-                    ['<C-Space>'] = require('cmp').mapping.complete(),
-                    ['<CR>'] = require('cmp').mapping.confirm {
-                        behavior = require('cmp').ConfirmBehavior.Replace,
-                        select = true,
-                    },
-                    ['<Tab>'] = require('cmp').mapping(function(fallback)
-                        if require('cmp').visible() then
-                            require('cmp').select_next_item()
-                            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-                            -- that way you will only jump inside the snippet region
-                        elseif require('luasnip').expand_or_jumpable() then
-                            require('luasnip').expand_or_jump()
-                        -- LeB: Prevent entering tab characters if the cursor is just after text...
-                        --elseif has_words_before() then
-                        --    require('cmp').complete()
-                        else
-                            fallback()
-                        end
-                    end, { 'i', 's' }),
+    --{
+    --    'hrsh7th/nvim-cmp',
+    --    event = { "InsertEnter", "CmdlineEnter" },
+    --    config = function()
+    --        require('cmp').setup({
+    --            snippet = {
+    --                expand = function(args)
+    --                    require('luasnip').lsp_expand(args.body)
+    --                end,
+    --            },
+    --            mapping = require('cmp').mapping.preset.insert {
+    --                ['<C-n>'] = require('cmp').mapping.select_next_item(),
+    --                ['<C-p>'] = require('cmp').mapping.select_prev_item(),
+    --                ['<C-d>'] = require('cmp').mapping.scroll_docs(-4),
+    --                ['<C-f>'] = require('cmp').mapping.scroll_docs(4),
+    --                ['<C-Space>'] = require('cmp').mapping.complete(),
+    --                ['<CR>'] = require('cmp').mapping.confirm {
+    --                    behavior = require('cmp').ConfirmBehavior.Replace,
+    --                    select = true,
+    --                },
+    --                ['<Tab>'] = require('cmp').mapping(function(fallback)
+    --                    if require('cmp').visible() then
+    --                        require('cmp').select_next_item()
+    --                        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+    --                        -- that way you will only jump inside the snippet region
+    --                    elseif require('luasnip').expand_or_jumpable() then
+    --                        require('luasnip').expand_or_jump()
+    --                    -- LeB: Prevent entering tab characters if the cursor is just after text...
+    --                    --elseif has_words_before() then
+    --                    --    require('cmp').complete()
+    --                    else
+    --                        fallback()
+    --                    end
+    --                end, { 'i', 's' }),
 
-                    ['<S-Tab>'] = require('cmp').mapping(function(fallback)
-                        if require('cmp').visible() then
-                            require('cmp').select_prev_item()
-                        elseif require('luasnip').jumpable(-1) then
-                            require('luasnip').jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, { 'i', 's' }),
-                },
-                sources = {
-                    { name = 'nvim_lsp' },
-                    { name = 'luasnip' },
-                },
-            })
-        end,
-        dependencies = {
-            -- Snippet Engine & its associated nvim-cmp source
-            {
-                'L3MON4D3/LuaSnip',
-                config = function()
-                    require("luasnip.loaders.from_vscode").lazy_load()
-                end
-            },
-            'saadparwaiz1/cmp_luasnip',
+    --                ['<S-Tab>'] = require('cmp').mapping(function(fallback)
+    --                    if require('cmp').visible() then
+    --                        require('cmp').select_prev_item()
+    --                    elseif require('luasnip').jumpable(-1) then
+    --                        require('luasnip').jump(-1)
+    --                    else
+    --                        fallback()
+    --                    end
+    --                end, { 'i', 's' }),
+    --            },
+    --            sources = {
+    --                { name = 'nvim_lsp' },
+    --                { name = 'luasnip' },
+    --            },
+    --        })
+    --    end,
+    --    dependencies = {
+    --        -- Snippet Engine & its associated nvim-cmp source
+    --        {
+    --            'L3MON4D3/LuaSnip',
+    --            config = function()
+    --                require("luasnip.loaders.from_vscode").lazy_load()
+    --            end
+    --        },
+    --        'saadparwaiz1/cmp_luasnip',
 
-            -- Adds LSP completion capabilities
-            'hrsh7th/cmp-nvim-lsp',
+    --        -- Adds LSP completion capabilities
+    --        'hrsh7th/cmp-nvim-lsp',
 
-            -- Adds a number of user-friendly snippets
-            'rafamadriz/friendly-snippets',
-        },
-    },
+    --        -- Adds a number of user-friendly snippets
+    --        'rafamadriz/friendly-snippets',
+    --    },
+    --},
 }
 
 return config
--- nvim/lua/plugins/configs/lsp.lua
+++ nvim/lua/plugins/configs/lsp.lua
@@ -29,11 +29,15 @@ local config = {
 			"williamboman/mason-lspconfig.nvim",
 			{
 				-- Neovim setup for init.lua and plugin development with full signature help, docs and completion for the nvim lua API.
-				"folke/neodev.nvim",
-				event = "LspAttach",
-				config = function()
-					require("neodev").setup()
-				end,
+				"folke/lazydev.nvim",
+				ft = "lua",
+				opts = {
+					library = {
+						-- See the configuration section for more details
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
 			},
 		},
 	},
@@ -50,8 +54,7 @@ local config = {
 
 			for config_server, config_opt in pairs(require("core.settings").lsp.servers) do
 				if not config_opt == false then
-					local capabilities = vim.lsp.protocol.make_client_capabilities()
-					capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+					local capabilities = require("blink.cmp").get_lsp_capabilities()
 					local opts = {
 						capabilities = capabilities,
 					}
@@ -74,6 +77,44 @@ local config = {
 		dependencies = {
 			"williamboman/mason.nvim",
 		},
+	},
+	{
+		"saghen/blink.cmp",
+		-- optional: provides snippets for the snippet source
+		dependencies = "rafamadriz/friendly-snippets",
+		-- use a release tag to download pre-built binaries
+		version = "*",
+		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+		-- build = 'cargo build --release',
+		-- If you use nix, you can build from source using latest nightly rust with:
+		-- build = 'nix run .#build-plugin',
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			-- 'default' for mappings similar to built-in completion
+			-- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+			-- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+			-- See the full "keymap" documentation for information on defining your own keymap.
+			keymap = {
+                preset = "default",
+                -- ['<S-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+            },
+			appearance = {
+				-- Sets the fallback highlight groups to nvim-cmp's highlight groups
+				-- Useful for when your theme doesn't support blink.cmp
+				-- Will be removed in a future release
+				use_nvim_cmp_as_default = true,
+				-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+				-- Adjusts spacing to ensure icons are aligned
+				nerd_font_variant = "mono",
+			},
+			-- Default list of enabled providers defined so that you can extend it
+			-- elsewhere in your config, without redefining it, due to `opts_extend`
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
+		},
+		opts_extend = { "sources.default" },
 	},
 	{
 		-- Show diagnostics messages in a window
@@ -112,10 +153,10 @@ local config = {
 					},
 					clang_format = {
 						-- Any additional configuration for Clang Format can go here
-                        -- Use format from a file
+						-- Use format from a file
 						args = {
-                            "--style=file:" .. vim.fn.expand("$HOME") .. "/.clang-format",
-                        }
+							"--style=file:" .. vim.fn.expand("$HOME") .. "/.clang-format",
+						},
 					},
 				},
 			})
