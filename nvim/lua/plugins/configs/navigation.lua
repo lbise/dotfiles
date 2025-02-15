784e183770aaf70986449c3ebfe820ee9ad2c31a
Léonard Bise <leonard.bise@gmail.com>
2025-02-15 14:56:45 +0100

    nvim: Add vim-gutentags

--- nvim/lua/plugins/configs/navigation.lua
+++ nvim/lua/plugins/configs/navigation.lua
@@ -1,8 +1,8 @@
 local function filenameFirst(_, path)
-	local tail = vim.fs.basename(path)
-	local parent = vim.fs.dirname(path)
-	if parent == "." then return tail end
-	return string.format("%s\t\t%s", tail, parent)
+    local tail = vim.fs.basename(path)
+    local parent = vim.fs.dirname(path)
+    if parent == "." then return tail end
+    return string.format("%s\t\t%s", tail, parent)
 end
 
 local config = {
@@ -26,13 +26,14 @@ local config = {
             "TmuxNavigatePrevious",
         },
         keys = {
-            { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-            { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-            { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-            { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+            { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
             { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
         },
     },
+    { 'ludovicchabant/vim-gutentags' },
 }
 
 return config
