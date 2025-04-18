5119c2899a14c067b1ad22278756d6651005cbb5
Léonard Bise <leonard.bise@gmail.com>
2025-04-18 16:37:28 +0200

    nvim: Fix copilot chat dep

--- nvim/lua/plugins/configs/ai.lua
+++ nvim/lua/plugins/configs/ai.lua
@@ -26,7 +26,7 @@ local config = {
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
 		dependencies = {
-			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "zbirenbaum/copilot.lua" },
 			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
 		},
 		build = "make tiktoken", -- Only on MacOS or Linux
