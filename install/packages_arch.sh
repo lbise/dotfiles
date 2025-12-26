29801c1a284eeec36fd51f3418a8cda344660289
Léonard Bise <leonard.bise@gmail.com>
2025-12-26 12:05:06 +0100

    install: Add new packages

--- install/packages_arch.sh
+++ install/packages_arch.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-PACMAN_PKGS="zsh tmux git unzip swaybg mako hypridle hyprlock man-db man-pages waybar impala"
-YAY_PKGS="neovim ghostty zen-browser-bin fzf ripgrep xdg-terminal-exec elephant walker"
+PACMAN_PKGS="zsh tmux git unzip swaybg mako hypridle hyprlock man-db man-pages waybar impala brightnessctl"
+YAY_PKGS="neovim ghostty zen-browser-bin fzf ripgrep xdg-terminal-exec elephant walker dropbox"
 
 # Only install packages if needed
 sudo pacman -S --noconfirm --needed $PACMAN_PKGS
