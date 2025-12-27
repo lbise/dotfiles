57b9c9406e310dad6bf412f35c11c4b23898dfea
Léonard Bise <leonard.bise@gmail.com>
2025-12-27 08:21:29 +0100

    scripts: Setup printer

--- scripts/system/system-setup-printer.sh
+++ scripts/system/system-setup-printer.sh
@@ -1,5 +1,33 @@
 #!/usr/bin/env bash
 
-sudo pacman -S cups
-sudo systemctl enable --now cups.service
+# Install required packages and GUI (system-config-printer)
+sudo pacman --needed -S cups system-config-printer cups-pdf ghostscript gsfonts
 sudo usermod -aG lp $USER
+
+sudo systemctl enable --now cups.service
+sudo systemctl restart cups.service
+
+# Install drivers
+yay -S canon-pixma-mg5200-complete
+
+echo "Looking for network printer..."
+PRINTER=$(cnijnetprn --installer --search auto)
+
+echo "Printer: $PRINTER"
+
+# Expected output
+# network cnijnet:/00-1E-8F-AD-3D-EC "Canon MG5200 series" "IP:192.168.1.28"
+
+# Extract the cnijnet address
+PRINTER_ADDRESS=$(echo "$PRINTER" | grep -oP 'cnijnet:[^ ]+' | head -n1)
+
+if [ -z "$PRINTER_ADDRESS" ]; then
+    echo "Error: Could not find printer address"
+    exit 1
+fi
+
+echo "Address: $PRINTER_ADDRESS"
+
+# Register printer
+echo "Registering printer..."
+sudo lpadmin -p MG5200 -m canonmg5200.ppd -v "$PRINTER_ADDRESS" -E
