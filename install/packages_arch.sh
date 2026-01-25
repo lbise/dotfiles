a7db502e5bc64f3aafdd10efb50f16481c371ebe
Léonard Bise <leonard.bise@gmail.com>
2026-01-25 12:10:00 +0100

    scripts: Add screenshot

--- install/packages_arch.sh
+++ install/packages_arch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-PACMAN_PKGS="zsh tmux git unzip swaybg mako hypridle hyprlock man-db man-pages waybar impala fcitx5 fcitx5-configtool fcitx5-gtk brightnessctl jq grim"
+PACMAN_PKGS="zsh tmux git unzip swaybg mako hypridle hyprlock man-db man-pages waybar impala fcitx5 fcitx5-configtool fcitx5-gtk brightnessctl jq grim satty"
 YAY_PKGS="neovim ghostty zen-browser-bin fzf ripgrep xdg-terminal-exec elephant-all-bin walker dropbox"
 
 # Only install packages if needed
