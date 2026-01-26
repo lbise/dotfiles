1d1d7c65fb7cfab292e12273e5810b9abfa4baa2
Léonard Bise <leonard.bise@gmail.com>
2026-01-26 10:41:52 +0100

    scripts: Add sound to RDP

--- scripts/connect_rdp.sh
+++ scripts/connect_rdp.sh
@@ -3,4 +3,6 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 RDP_FILE="${SCRIPT_DIR}/CH03CWA69S5TB4.rdp"
 
-xfreerdp3 "${RDP_FILE}" /u:"AzureAD\leonard.bise@sonova.com" /d:sonova.com
+xfreerdp3 "${RDP_FILE}" /u:"AzureAD\leonard.bise@sonova.com" /d:sonova.com /sound
+
+# Trace level: /log-level:TRACE
