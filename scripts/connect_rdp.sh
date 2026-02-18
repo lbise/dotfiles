6218b6f363bcd5cbd65af7a2328eed42438d959d
Léonard Bise <leonard.bise@gmail.com>
2026-02-18 14:44:39 +0100

    scripts: Add dummy password to connect_rdp

--- scripts/connect_rdp.sh
+++ scripts/connect_rdp.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 RDP_FILE="${SCRIPT_DIR}/CH03CWA69S5TB4.rdp"
 
-xfreerdp3 "${RDP_FILE}" /u:"AzureAD\leonard.bise@sonova.com" /d:sonova.com /sound
+# Password is not used here, have to login again in RDP. Just pass anything so freerdp does not ask
+xfreerdp3 "${RDP_FILE}" /u:"AzureAD\leonard.bise@sonova.com" /p:tutu /d:sonova.com /sound
 
 # Trace level: /log-level:TRACE
