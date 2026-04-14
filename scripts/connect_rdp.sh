d94893e384b3d21ff4ad38a0a2e9658e80e70006
Léonard Bise <leonard.bise@gmail.com>
2026-04-14 08:50:52 +0200

    scripts: Update connect_rdp after loss of discovery

--- scripts/connect_rdp.sh
+++ scripts/connect_rdp.sh
@@ -4,6 +4,9 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 RDP_FILE="${SCRIPT_DIR}/CH03CWA69S5TB4.rdp"
 
 # Password is not used here, have to login again in RDP. Just pass anything so freerdp does not ask
-xfreerdp3 "${RDP_FILE}" /u:"AzureAD\leonard.bise@sonova.com" /p:tutu /d:sonova.com /sound
+# Derived from scripts/CH03CWA69S5TB4.rdp
+xfreerdp3 /v:CH03CWA69S5TB4  /u:"AzureAD\leonard.bise@sonova.com" /p:tutu /d:sonova.com /sound /log-level:TRACE /sec:nla:off /f
 
 # Trace level: /log-level:TRACE
+# /sec:nla:off Disable NLA auth (Required for Sonova)
+# /f Full screen
